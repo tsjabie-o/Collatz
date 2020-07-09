0693c4959ad0a370d68596db6b341f7d89bcd0c6
Xavi Oorthuis <x.oorthuis@outlook.com>
2020-07-09 13:55:17 +0200

    Initial commit

---
+++ Opdracht 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Opdracht_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine().Split(' ')[0]);
+
+            string[] regels = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                regels[i] = Console.ReadLine();
+            }
+
+            List<int> collatzLengths = new List<int>();
+
+            foreach (string regel in regels)
+            {
+                long getal = long.Parse(regel.Split(' ')[0]);
+
+                int collatzLengte = 0;
+
+                while (getal > 1)
+                {
+                    if (getal % 2 == 0)
+                    {
+                        getal /= 2;
+                        collatzLengte++;
+                    }
+                    else
+                    {
+                        getal *= 3;
+                        getal++;
+                        collatzLengte++;
+                    }
+                }
+                collatzLengths.Add(collatzLengte);
+            }
+
+            foreach (int collatzlengte in collatzLengths)
+            {
+                Console.WriteLine(collatzlengte);
+            }
+        }
+    }
+}
